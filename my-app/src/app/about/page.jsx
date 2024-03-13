d3c903d62d24552958af55cca4201fe6846f12e8
kellymweu <mweukelly@gmail.com>
2024-03-13 06:16:42 +0300

    fixed an error

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -4,8 +4,8 @@ import { NavLinks } from "../../../constants";
 const About = () => {
   return (
     <div className="p-28 bg-red-600">
-      <h1 className="text-gray-600 text-bold text-4xl ">About Us</h1>
-      <p className="text-5xl text-bold text-red-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, vero rem quod possimus saepe ipsa, error doloribus dolores voluptas eum dicta tempore ipsum vel. Laudantium vitae doloremque veritatis aliquam quod!</p>
+      <h1 className="text-blue-600 text-bold text-4xl ">About Us</h1>
+      <p className="text-5xl text-bold hover:text-red-600 text-blue-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, vero rem quod possimus saepe ipsa, error doloribus dolores voluptas eum dicta tempore ipsum vel. Laudantium vitae doloremque veritatis aliquam quod!</p>
     </div>
   );
 };

2a62a32e7e46f8c64156a0143b6d30b9db7b14bb
kellymweu <mweukelly@gmail.com>
2024-03-17 10:57:03 +0300

    made the Article form

--- my-app/lib/actions/formAction.js
+++ my-app/lib/actions/formAction.js
@@ -1,18 +1,18 @@
 "use server";
 //server action as it sends info to the DB
 
-import { PrismaClient } from "@prisma/client";
-import { revalidatePath } from "next/cache";
+import { PrismaClient } from "@prisma/client"; //https://www.prisma.io/client
+import { revalidatePath } from "next/cache"; //https://nextjs.org/docs/app/api-reference/functions/revalidatePath
 
 const prisma = new PrismaClient();
 
 //if ts is used, say (FormData: FormData)
-export const addArticle = async (FormData) => {
-  const articleTitle = FormData.get("articleTitle");
-  const articleBody = FormData.get("articleBody");
+export const addArticle = async (formData) => {
+  const articleTitle = formData.get("articleTitle");
+  const articleBody = formData.get("articleBody");
 
   //MUTATIONS
-  const article = await prisma.article.create({
+  const Article = await prisma.article.create({
     data: {
       title: articleTitle,
       body: articleBody,

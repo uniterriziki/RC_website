dbfbde0454e12ec44fd605ba346ee9b686a6e70c
kellymweu <mweukelly@gmail.com>
2024-03-17 21:05:55 +0300

    feat(Auth): Next-Auth

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,35 +1,48 @@
+"use client";
+
 import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import ArticleForm from "../../components/ArticleForm";
+import { useSession } from "next-auth/react";
 
-const prisma = new PrismaClient();
+import React from "react";
 
-const writeArticlePage = async () => {
-  //const articles = await prisma.articles.findMany();
-
-  const addArticle = async (formData) => {
-    "use server"; //using server actions here to handle all api calls in nextjs
-
-    //store data from the form in these constants at first using get method.
-    const articleTitle = formData.get("articleTitle");
-    const articleBody = formData.get("articleBody");
-
-    //do server mutations to pick new data, using prisma ORM and calling Mongo DB
-    const article = await prisma.articles.create({
-      data: {
-        title: articleTitle, //if TS is used, declare type as string
-        body: articleBody,
-      },
-    });
-
-    revalidatePath("/write");
-  };
-  return (
-    <div>
-      <h1>Write a new article here!</h1>
-      <ArticleForm />
-    </div>
-  );
+const page = () => {
+  const { data: session } = useSession();
+  if(!session || !session.user) return  ()
+  return <div>protect this route</div>;
 };
 
-export default writeArticlePage;
+export default page;
+
+// const prisma = new PrismaClient();
+
+// const writeArticlePage = async () => {
+//   const articles = await prisma.articles.findMany();
+
+//   const addArticle = async (formData) => {
+//     "use server"; //using server actions here to handle all api calls in nextjs
+
+//     //store data from the form in these constants at first using get method.
+//     const articleTitle = formData.get("articleTitle");
+//     const articleBody = formData.get("articleBody");
+
+//     //do server mutations to pick new data, using prisma ORM and calling Mongo DB
+//     const article = await prisma.articles.create({
+//       data: {
+//         title: articleTitle, //if TS is used, declare type as string
+//         body: articleBody,
+//       },
+//     });
+
+//     revalidatePath("/write");
+//   };
+//   return (
+//     <div>
+//       <h1>Write a new article here!</h1>
+//       <ArticleForm />
+//     </div>
+//   );
+// };
+
+// export default writeArticlePage;

6d4608a7463c584667e27cc36eebf72c6e09dedb
kellymweu <mweukelly@gmail.com>
2024-03-18 22:03:13 +0300

    feat(rich-text editor): created the editor components

--- components/richtext-ui/ArticlePicker.jsx
+++ components/richtext-ui/ArticlePicker.jsx
@@ -2,21 +2,35 @@
 
 import Tiptap from "./Tiptap";
 import React, { useState } from "react";
+import { uuidv4 } from "uuid";
 
 const ArticlePicker = () => {
   const [content, setContent] = useState("");
   const handleContentChange = (reason) => {
     setContent(reason);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      id: 1, //Universally unique identifier, is 36 bit BSON type
+      content: content,
+    };
+    console.log(data); //check if data is actually being pushed locally
+  };
+
   return (
     <div>
-      <form className="max-w-3xl w-full grid place-items-center mx-auto pt-10 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-3xl w-full grid place-items-center mx-auto pt-10 mb-10"
+      >
         <div className="text-3xl text-center text-sky-300 mb-10">
           Article Picker
         </div>
         <Tiptap
           content={content}
-          onChange={(nexContent) => handleContentChange(newContent)}
+          onChange={(newContent) => handleContentChange(newContent)}
         />
       </form>
     </div>
--- components/richtext-ui/Tiptap.jsx
+++ components/richtext-ui/Tiptap.jsx
@@ -3,17 +3,19 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Toolbar from "./Toolbar";
+import Underline from "@tiptap/extension-underline"; //standalone tiptap lib for underline
+import Blockquote from "@tiptap/extension-blockquote";
 
 const Tiptap = ({ onChange, content }) => {
   const handleContentChange = (newContent) => {
     onChange(newContent);
   };
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, Underline, Blockquote],
     editorProps: {
       attributes: {
         className:
-          " flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+          "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full h-10 gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md",
       },
     },
     onUpdate: ({ editor }) => {
@@ -24,7 +26,11 @@ const Tiptap = ({ onChange, content }) => {
   return (
     <div className="w-full px-4">
       <Toolbar editor={editor} content={content} />
-      <EditorContent style={{ whiteSpace: "pre-line" }} editor={editor} />
+      <EditorContent
+        style={{ whiteSpace: "pre-line" }}
+        editor={editor}
+        className="border-gray-800 border-2"
+      />
     </div>
   );
 };

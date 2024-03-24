1495d5ad24aff4f1fd4c654b36b70ebac280aada
kellymweu <mweukelly@gmail.com>
2024-03-24 13:30:57 +0300

    ui:Navbar styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,20 +12,22 @@ const Navbar = async () => {
 
   return (
     <nav className="bg-white h-16 md:flex md:items-end md:justify-between ">
-      <Link href="/">
-        <Image
-          src={"/medical logo.jpg"}
-          height={115}
-          width={85}
-          alt="Medical Club Logo"
-        />
-      </Link>
+      <div className="ml-10">
+        <Link href="/">
+          <Image
+            src={"/medical logo.jpg"}
+            height={115}
+            width={85}
+            alt="Medical Club Logo"
+          />
+        </Link>
+      </div>
 
       <div className="justify-between gap-10 px-46">
-        <ul className="md:flex hidden text-base gap-6 justify-end ">
+        <ul className="md:flex hidden text-sm gap-6 justify-end ">
           {NavLinks.map((link) => (
             <Link
-              className="hover:text-red-500 duration-500 hover:shadow-xl"
+              className="text-black hover:text-blue-300 rounded-xl p-2 duration-500 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
               href={link.href}
               key={link.key}
             >
@@ -33,7 +35,7 @@ const Navbar = async () => {
             </Link>
           ))}
           <Link
-            className=" px-4 flex text-white text-semibold hover:text-red-500 duration-500 hover:shadow-xl bg-blue-400 rounded-xl"
+            className="h-6 px-4 flex text-blue-400 text-bold hover:text-blue-800 duration-500 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] border-2 border-blue-400 rounded-xl"
             href={"/signup"}
           >
             Sign Up

4fed5530a7b94f97b964ed6784222319c31f0f29
kellymweu <mweukelly@gmail.com>
2024-10-22 21:18:05 +0300

    changes the position of the footer copyright

--- app/values/page.jsx
+++ app/values/page.jsx
@@ -90,19 +90,7 @@ export default function Values() {
           </div>
         </section>
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">
-          © 2024 Your Company Name. All rights reserved.
-        </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <a className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
-          </a>
-          <a className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </a>
-        </nav>
-      </footer>
+      
     </div>
   );
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -67,10 +67,23 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className=" flexBetween">
+      {/* <div className=" flexBetween">
         <p className="text-center italic text-sm font-bold">
           ©2024 RC-UON. All Rights Reserved
         </p>
+      </div> */}
+      <div className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-muted-foreground">
+          © 2024 RC UON. All rights reserved.
+        </p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <a className="text-xs hover:underline underline-offset-4" href="#">
+            Terms of Service
+          </a>
+          <a className="text-xs hover:underline underline-offset-4" href="#">
+            Privacy
+          </a>
+        </nav>
       </div>
     </footer>
   );
